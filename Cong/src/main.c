e029507c3627e0d5bbe1c50b4f887f53c756c7d5
skkra <skkra45@gmail.com>
2023-10-13 18:14:49 -0400

    setup

---
+++ Cong/src/main.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, world!\n");
+}
---
+++ Cong/src/main.h
@@ -0,0 +1,6 @@
+#ifndef CONG_MAIN_H
+#define CONG_MAIN_H
+
+#include <raylib.h>
+
+#endif
