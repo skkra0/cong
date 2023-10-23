83f5110ec60e8020ae1df3b2af40d46f97521298
skkra0 <73250770+skkra0@users.noreply.github.com>
2023-10-23 13:06:49 -0400

    Update ball.c

--- Cong/src/ball.c
+++ Cong/src/ball.c
@@ -8,3 +8,7 @@ void updateBall(Ball* ball) {
 void drawBall(Ball* ball) {
   DrawCircleV(ball->position, ball->radius, RAYWHITE);
 }
+
+bool detectBallHitY(Ball* ball) {
+  return ball->position.y <= ball->radius || ball->position.y >= GetScreenHeight() - ball->radius;
+}
