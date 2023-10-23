9e12c65c8ea609e2a764d2705468e98f5418e35e
skkra <skkra45@gmail.com>
2023-10-22 22:49:07 -0400

    pad bounds

--- Cong/src/pad.c
+++ Cong/src/pad.c
@@ -1,10 +1,10 @@
 #include "pad.h"
 
 void updatePad(Pad* pad) {
-    if (IsKeyDown(pad->downBtn)) {
+    if (IsKeyDown(pad->downBtn) && (pad->position.y + pad->size.y) < GetScreenHeight()) {
         pad->position.y += pad->speed;
     }
-    if (IsKeyDown(pad->upBtn)) {
+    if (IsKeyDown(pad->upBtn) && pad->position.y > 0) {
         pad->position.y -= pad->speed;
     }
 }
